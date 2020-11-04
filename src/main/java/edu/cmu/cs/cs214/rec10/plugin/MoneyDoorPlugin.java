ab176153ee5002eff7201889d477abbb30280af4
xinzhu-cai <hzh13528@163.com>
2020-11-04 16:34:01 -0500

    Constant style

--- src/main/java/edu/cmu/cs/cs214/rec10/plugin/MoneyDoorPlugin.java
+++ src/main/java/edu/cmu/cs/cs214/rec10/plugin/MoneyDoorPlugin.java
@@ -9,8 +9,8 @@ import java.util.*;
 public class MoneyDoorPlugin implements GamePlugin {
 
     private int[] arr = {-3, -2, -1, 0, 1, 2, 3, 4, 5, 6};
-    private int ONE = 1;
-    private int TEN = 10;
+    private final int ONE = 1;
+    private final int TEN = 10;
 
     // The game framework
     private GameFramework framework = null;
